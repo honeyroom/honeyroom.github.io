58c49bb464b4abb067289dc05f53eb184b2fcd12
honeyroom <xkhjr@mailfs.com>
2017-02-03 09:05:40 +0200

    initial

--- bash-test.sh
+++ bash-test.sh
@@ -3,7 +3,9 @@
 username=honeyroom
 fullname="Ann Gordon"
 email="xkhjr@mailfs.com"
+echo "----------"
 echo '"ssh -i ../../deployment-keys/'$username'_id_rsa -F /dev/null"'
+echo "----------"
 git config core.sshCommand '"ssh -i ../../deployment-keys/'$username'_id_rsa -F /dev/null"'
 git config user.email '"'$email'"'
 git config user.name '"'$username'"'
