ecf07b659696d08ae2fcaa7e59cdf210932e2c8c
honeyroom <xkhjr@mailfs.com>
2017-02-03 09:07:54 +0200

    initial

--- bash-test.sh
+++ bash-test.sh
@@ -7,6 +7,7 @@ echo "----------"
 echo '"ssh -i ../../deployment-keys/'$username'_id_rsa -F /dev/null"'
 echo "----------"
 cat '../../deployment-keys/'$username'_id_rsa'
+echo 'git config core.sshCommand "ssh -i ../../deployment-keys/'$username'_id_rsa -F /dev/null"'
 git config core.sshCommand '"ssh -i ../../deployment-keys/'$username'_id_rsa -F /dev/null"'
 git config user.email '"'$email'"'
 git config user.name '"'$username'"'
